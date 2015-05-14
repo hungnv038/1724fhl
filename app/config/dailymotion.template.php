57ce96560ef19ab0663bad9582693b97ccb3a770
Michael <hungnv038@gmail.com>
2015-05-15 00:03:51 +0700

    supported upload video to dailymotion

---
+++ app/config/dailymotion.template.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michael
+ * Date: 5/14/15
+ * Time: 15:19
+ */
+return array(
+    'api'=>'key',
+    'secret'=>'secret',
+    'user'=>'user name',
+    'password'=>'password'
+);
